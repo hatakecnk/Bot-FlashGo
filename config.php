6c2c32c6ac357a9e13094e07724a15cdfee97e44
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-02-02 12:59:43 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+// configurasi //
+// ganti semuanya menurut data punya kalian"
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "78e22fc7-c634-4328-95b5-428cf673ff91";
+//user_token wajib di isi //
+$user_token="AbyYe22YMB_Wztdn";
+
+//user_id wajib di isi //
+$user_id="Facebook_1248245938666034";
+
+// invite_code wajib di isi //
+$invite_code="F5iS4K5vqU";
+
+// Bearer wajib di isi //
+$Bearer="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xMjQ4MjQ1OTM4NjY2MDM0IiwiZXhwIjoiMTU1MTY2Mjg4Nzk4MSJ9.Hab50hydvzhzhsGiPvQn9qBaDcmIVTZrd0s7N6FJ0AA";
+
+?>
